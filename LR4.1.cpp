51421e3e30d67a8a1287839c417538a39fac94be
TatarskyiVasyl <tatarskyi.vasyl.kn@dspu.edu.ua>
2025-10-18 21:24:30 +0300

    Add files via upload

---
+++ LR4.1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int k, N;
+    double S;
+
+    cout << "N = "; cin >> N;
+
+    S = 0;
+    k = N;
+    while (k <= 19)
+    {
+        S += sqrt(sin(k) * sin(k) + cos(N / (double)k) * cos(N / (double)k));
+        k++;
+    }
+    cout << S << endl;
+
+    S = 0;
+    k = N;
+    do {
+        S += sqrt(sin(k) * sin(k) + cos(N / (double)k) * cos(N / (double)k));
+        k++;
+    } while (k <= 19);
+    cout << S << endl;
+
+    S = 0;
+    for (k = N; k <= 19; k++)
+    {
+        S += sqrt(sin(k) * sin(k) + cos(N / (double)k) * cos(N / (double)k));
+    }
+    cout << S << endl;
+
+    S = 0;
+    for (k = 19; k >= N; k--)
+    {
+        S += sqrt(sin(k) * sin(k) + cos(N / (double)k) * cos(N / (double)k));
+    }
+    cout << S << endl;
+
+    return 0;
+}
